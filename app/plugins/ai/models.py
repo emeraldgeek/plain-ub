f540e6564ca3babf0ef78514184fc7d8e635fcef
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2024-04-20 22:22:35 +0530

    Personalization to models.

--- app/plugins/ai/models.py
+++ app/plugins/ai/models.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-from app import Message, extra_config
+from app import Message, extra_config, message
 
 
 async def init_task():
@@ -8,15 +8,37 @@ async def init_task():
         genai.configure(api_key=extra_config.GEMINI_API_KEY)
 
 
-GENERATION_CONFIG = {"temperature": 0.69, "max_output_tokens": 2048}
+GENERATION_CONFIG = {"temperature": 0.75, "max_output_tokens": 3096}
 
 SAFETY_SETTINGS = [
-    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_ONLY_HIGH"},
-    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_ONLY_HIGH"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_ONLY_HIGH"},
-    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_ONLY_HIGH"},
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 
+PAST = []
+HISTORY = []
+SPECIFIC_GROUP_ID = [-1001898736703, -1002010754513]
+CONTEXT = []
+if message.chat.id in SPECIFIC_GROUP_ID:
+    CONTEXT = PAST
+else:
+    CONTEXT = HISTORY
+
+@bot.add_cmd(cmd="fh")
+async def fetch_history(bot=bot, message=None):
+    history_message_id = int(os.environ.get("HISTORY_MESSAGE_ID"))
+    past_message_id = int(os.environ.get("PAST_MESSAGE_ID"))
+    history_message, past_message = await bot.get_messages(
+        chat_id=Config.LOG_CHAT, message_ids=[history_message_id, past_message_id]
+    )
+    global HISTORY
+    HISTORY = json.loads(history_message.text)
+    global PAST
+    PAST = json.loads(past_message.text)
+    if message is not None:
+        await message.reply("Done.")
 
 TEXT_MODEL = genai.GenerativeModel(
     model_name="gemini-pro",
@@ -31,11 +53,17 @@ IMAGE_MODEL = genai.GenerativeModel(
 )
 
 MEDIA_MODEL = genai.GenerativeModel(
-    model_name="models/gemini-1.5-pro-latest",
+    model_name="gemini-1.5-pro-latest",
     generation_config=GENERATION_CONFIG,
     safety_settings=SAFETY_SETTINGS,
 )
 
+ONEFIVE = genai.GenerativeModel(
+    model_name="gemini-1.5-pro-latest",
+    generation_config=GENERATION_CONFIG,
+    system_instruction=CONTEXT,
+    safety_settings=SAFETY_SETTINGS,
+)
 
 async def basic_check(message: Message):
     if not extra_config.GEMINI_API_KEY:
@@ -45,7 +73,7 @@ async def basic_check(message: Message):
             "and set GEMINI_API_KEY var."
         )
         return
-    if not message.input:
+    if not message.input and not message.replied:
         await message.reply("Ask a Question.")
         return
     return 1
