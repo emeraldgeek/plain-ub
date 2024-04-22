ae5277b8e2de830adfad9f9ee48452188f48b312
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2024-04-22 22:58:19 +0530

    Create music_status.py

---
+++ app/plugins/misc/music_status.py
@@ -0,0 +1,33 @@
+from app import bot, Message
+
+@bot.add_cmd(cmd="sp")
+async def lastfm_status(bot, message: Message):
+    try:
+        results = await bot.get_inline_bot_results("spotipiebot", "now")
+        if results.results:
+            first_result = results.results[0]
+            await message.reply_inline_bot_result(
+                query_id=results.query_id,
+                result_id=first_result.id
+            )
+        else:
+            await message.reply("No results found.")
+
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
+
+@bot.add_cmd(cmd="st")
+async def lastfm_status(bot, message: Message):
+    try:
+        results = await bot.get_inline_bot_results("lastfmrobot", "status")
+        if results.results:
+            first_result = results.results[0]
+            await message.reply_inline_bot_result(
+                query_id=results.query_id,
+                result_id=first_result.id
+            )
+        else:
+            await message.reply("No results found.")
+
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
