fd9f7b700601d061c46fe5935346017b3bbe4a63
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2024-04-27 09:34:49 +0530

    Update and rename music_status.py to inline_cmds.py

--- app/plugins/misc/inline_cmds.py
+++ app/plugins/misc/inline_cmds.py
@@ -31,3 +31,19 @@ async def lastfm_status(bot, message: Message):
 
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
+
+@bot.add_cmd(cmd="dl")
+async def social_dl(bot, message: Message):
+    try:
+        results = await bot.get_inline_bot_results("rsdl_bot", message.input)
+        if results.results:
+            first_result = results.results[0]
+            await message.reply_inline_bot_result(
+                query_id=results.query_id,
+                result_id=first_result.id
+            )
+        else:
+            await message.reply("No results found.")
+
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
